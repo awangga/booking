d29a0eb9237dfa3915a0bee1b8b2b232bfc8838e
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-03 01:03:12 +0700

    create user profile and forget password table

--- app/database/migrations/2015_06_02_175542_create_users_table.php
+++ app/database/migrations/2015_06_02_175542_create_users_table.php
@@ -12,7 +12,36 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('users', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('profile_id');
+			$table->string('domain_id', 255)->nullable();
+			$table->string('email', 255);
+			$table->string('username', 255);
+			$table->string('password', 255);
+			$table->timestamp('created_at');
+			$table->timestamp('updated_at');
+			$table->timestamp('deleted_at')->nullable();
+			$table->integer('status');
+			$table->integer('flag_banned')->default(0);
+			$table->string('remember_token', 100)->nullable();
+		});
+		
+		Schema::create('profiles', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('first_name', 255);
+			$table->string('last_name', 255);
+			$table->string('website', 255);
+			$table->timestamp('created_at');
+			$table->timestamp('updated_at');
+			$table->timestamp('deleted_at')->nullable();
+		});
+		
+		Schema::create('password_resets', function (Blueprint $table) {
+			$table->string('email')->index();
+			$table->string('token')->index();
+			$table->timestamp('created_at');
+		});
 	}
 
 	/**
@@ -22,7 +51,9 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::dropIfExists('users');
+		Schema::dropIfExists('profiles');
+		Schema::dropIfExists('password_resets');
 	}
 
 }
