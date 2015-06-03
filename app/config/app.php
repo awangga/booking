63914e2448c4525e2a47533c3b5f8c19f8b9ac5e
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-03 16:46:25 +0700

    add email confimation vendor

--- app/config/app.php
+++ app/config/app.php
@@ -94,7 +94,7 @@ return array(
 	*/
 
 	'providers' => array(
-
+		'Aamant\EmailConfirmation\EmailConfirmationServiceProvider',
 		'Illuminate\Foundation\Providers\ArtisanServiceProvider',
 		'Illuminate\Auth\AuthServiceProvider',
 		'Illuminate\Cache\CacheServiceProvider',
